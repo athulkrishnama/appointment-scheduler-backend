216e0e715c157b06424bc0ef58a5c26a6cade61b
athulkrishnama <athulkrishnama24@gmail.com>
2024-12-27 09:52:40 +0530

    signup route added

--- routes/authRoute.js
+++ routes/authRoute.js
@@ -1,4 +1,6 @@
 const express = require('express');
 const authRoute = express.Router();
+const authController = require('../controllers/authController');
 
+authRoute.post('/signup', authController.signup);
 module.exports = authRoute
