0ffde0af8a9904f65441de024f7ec86b70a8a482
athulkrishnama <athulkrishnama24@gmail.com>
2024-12-27 09:52:14 +0530

    store origins

--- constants/origins.js
+++ constants/origins.js
@@ -1,6 +1,6 @@
 const ORIGIN = {
-    client:'http://localhost:5173/',
-    service:'http://localhost:5174/',
-    admin:'http://localhost:5175/',
+    client:'http://localhost:5173',
+    service:'http://localhost:5174',
+    admin:'http://localhost:5175',
 }
 module.exports = ORIGIN;
