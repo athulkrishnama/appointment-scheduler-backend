77c0c9f2179da92d5ec1a1a6500f340e6addada2
athulkrishnama <athulkrishnama24@gmail.com>
2024-12-30 09:28:12 +0530

    routes created for login and google signup

--- routes/authRoute.js
+++ routes/authRoute.js
@@ -5,4 +5,8 @@ const authController = require('../controllers/authController');
 authRoute.post('/signup', authController.signup);
 authRoute.post('/verify-otp', authController.verifyOtp);
 authRoute.get('/resend-otp', authController.resendOtp);
+
+authRoute.post('/login', authController.login);
+
+authRoute.post('/google-signup', authController.googleSignup);
 module.exports = authRoute
