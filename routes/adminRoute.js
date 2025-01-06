9d94cda426f28b96254b83e3c8af0dc0c9f29ba4
athulkrishnama <athulkrishnama24@gmail.com>
2025-01-06 18:22:34 +0530

    service proivder listing and chaning status implemented and auth middleware added

--- routes/adminRoute.js
+++ routes/adminRoute.js
@@ -1,12 +1,16 @@
 const express = require("express");
 const adminRoute = express.Router();
 const adminController = require("../controllers/adminController");
-
+const {verifyToken} = require("../middlewares/auth")
 // serviece provider requests
-adminRoute.get("/serviceProviderRequests", adminController.serviceProviderRequests);
-adminRoute.patch("/updateRequestStatus", adminController.updateRequestStatus);
+adminRoute.get("/serviceProviderRequests", verifyToken, adminController.serviceProviderRequests);
+adminRoute.patch("/updateRequestStatus", verifyToken, adminController.updateRequestStatus);
 
 // client management
-adminRoute.get("/clients", adminController.getClients);
-adminRoute.patch("/updateClientStatus/:id", adminController.updateClientStatus);
+adminRoute.get("/clients", verifyToken, adminController.getClients);
+adminRoute.patch("/updateClientStatus/:id", verifyToken, adminController.updateClientStatus);
+
+// service provider management
+adminRoute.get("/service-providers",verifyToken, adminController.getServiceProvider);
+adminRoute.patch("/updateServiceProviderStatus/:id", verifyToken, adminController.updateServiceProviderStatus);
 module.exports = adminRoute
