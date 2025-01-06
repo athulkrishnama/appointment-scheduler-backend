bcfee0d74006ee673b9c72546e12a8d24653364e
athulkrishnama <athulkrishnama24@gmail.com>
2025-01-06 19:38:09 +0530

    user verification middleware created

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -1,16 +1,22 @@
 const jwt = require('jsonwebtoken');
-
-const verifyToken = (req, res, next) => {
+const User = require('../models/user');
+const verifyToken =async (req, res, next) => {
   const token = req.headers['authorization'];
   if (!token) {
     return res.status(403).json({ success: false, message: 'No token provided.' });
   }
-
-  jwt.verify(token.split(' ')[1], process.env.JWT_SECRET, (err, decoded) => {
+  jwt.verify(token.split(' ')[1], process.env.ACCESS_TOKEN_SECRET, async(err, decoded) => {
     if (err) {
       return res.status(401).json({ success: false, message: 'Failed to authenticate token.' });
     }
     req.userId = decoded.id;
+    const user = await User.findById(req.userId);
+    if (!user) {
+      return res.status(404).json({ success: false, message: 'User not found.' });
+    }
+    if(user.isActive === false){
+      return res.status(400).json({ success: false, message: 'User is blocked.' });
+    }
     next();
   });
 };
