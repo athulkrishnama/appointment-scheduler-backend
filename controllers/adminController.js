7e9d0c6bc1f64386aa9d0a73ff0b4d8335342e91
athulkrishnama <athulkrishnama24@gmail.com>
2025-01-06 15:07:07 +0530

    create routes for listing and blocking and unblocking of clients

--- controllers/adminController.js
+++ controllers/adminController.js
@@ -4,7 +4,9 @@ const ROLES = require("../constants/roles");
 
 const serviceProviderRequests = async (req, res) => {
     try {
-        const requests = await User.find({ "role": ROLES.SERVICE, "serviceDetails.isAccepted": STATUSES.PENDING });
+        const page = parseInt(req.query.page) || 1;
+        const limit = parseInt(req.query.limit) || 5;
+        const requests = await User.find({ "role": ROLES.SERVICE, "serviceDetails.isAccepted": STATUSES.PENDING }).skip((page - 1) * limit).limit(limit);
         res.status(200).json({ success: true, requests });
     } catch (err) {
         console.log(err);
@@ -27,4 +29,35 @@ const updateRequestStatus = async (req, res) => {
         res.status(500).json({ success: false, message: "Failed to update request status" });
     }
 };
-module.exports = { serviceProviderRequests, updateRequestStatus };
+
+
+const getClients = async (req, res) => {
+    try {
+        const page = parseInt(req.query.page) || 1;
+        const limit = parseInt(req.query.limit) || 5;
+        const clients = await User.find({ "role": ROLES.CLIENT }).skip((page - 1) * limit).limit(limit);
+        const totalPage = Math.ceil(await User.countDocuments({ "role": ROLES.CLIENT }) / limit);
+        res.status(200).json({ success: true, clients, totalPage, curPage: page });
+    } catch (err) {
+        console.log(err);
+        res.status(500).json({ success: false, message: "Failed to fetch clients" });
+    }
+};
+
+const updateClientStatus = async (req, res) => {
+    try {
+        const {id} = req.params;
+        const { action } = req.body;
+        const user = await User.findById(id);
+        if (!user) {
+            return res.status(404).json({ success: false, message: "Client not found" });
+        }
+        user.isActive = action === "block" ? false : true;
+        await user.save();
+        res.status(200).json({ success: true, message: "Client status updated successfully" });
+    } catch (err) {
+        console.log(err);
+        res.status(500).json({ success: false, message: "Failed to update client status" });
+    }
+};
+module.exports = { serviceProviderRequests, updateRequestStatus , getClients, updateClientStatus};
--- routes/adminRoute.js
+++ routes/adminRoute.js
@@ -2,6 +2,11 @@ const express = require("express");
 const adminRoute = express.Router();
 const adminController = require("../controllers/adminController");
 
+// serviece provider requests
 adminRoute.get("/serviceProviderRequests", adminController.serviceProviderRequests);
 adminRoute.patch("/updateRequestStatus", adminController.updateRequestStatus);
+
+// client management
+adminRoute.get("/clients", adminController.getClients);
+adminRoute.patch("/updateClientStatus/:id", adminController.updateClientStatus);
 module.exports = adminRoute
