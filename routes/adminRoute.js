274e4d71278c289903af11508c1db0571270b074
athulkrishnama <athulkrishnama24@gmail.com>
2025-01-07 19:35:56 +0530

    route added for category management

--- routes/adminRoute.js
+++ routes/adminRoute.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const adminRoute = express.Router();
 const adminController = require("../controllers/adminController");
+const categoryController = require("../controllers/categoryController");
 const {verifyToken} = require("../middlewares/auth")
 // serviece provider requests
 adminRoute.get("/serviceProviderRequests", verifyToken, adminController.serviceProviderRequests);
@@ -13,4 +14,12 @@ adminRoute.patch("/updateClientStatus/:id", verifyToken, adminController.updateC
 // service provider management
 adminRoute.get("/service-providers",verifyToken, adminController.getServiceProvider);
 adminRoute.patch("/updateServiceProviderStatus/:id", verifyToken, adminController.updateServiceProviderStatus);
+
+// category management
+adminRoute.get("/categories", verifyToken, categoryController.getCategories);
+adminRoute.post("/addCategory", verifyToken, categoryController.addCategory);
+adminRoute.patch("/updateCategoryStatus/:id", verifyToken, categoryController.updateCategoryStatus);
+// adminRoute.delete("/deleteCategory/:id", verifyToken, adminController.deleteCategory);
+adminRoute.put("/updateCategory/:id", verifyToken, categoryController.updateCategory);
+
 module.exports = adminRoute
