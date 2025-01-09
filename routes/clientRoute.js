a9ba0afc8d48051a87ecbba86750c1b3c51e8f29
athulkrishnama <athulkrishnama24@gmail.com>
2025-01-10 03:35:22 +0530

    created routes to get routes and get filter data

--- routes/clientRoute.js
+++ routes/clientRoute.js
@@ -3,4 +3,8 @@ const clientRoute = express.Router()
 const clientController = require('../controllers/clientController')
 clientRoute.get('/getTopServices', clientController.getTopServices)
 
+// getServices
+clientRoute.get('/services', clientController.getServices);
+clientRoute.get('/getFilterData', clientController.getFilterData);
+
 module.exports = clientRoute
