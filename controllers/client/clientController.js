52804812f70a8028fab3cfa3c4f5af552e0a7948
athulkrishnama <athulkrishnama24@gmail.com>
2025-01-17 12:09:16 +0530

    created controller for update profile

--- controllers/client/clientController.js
+++ controllers/client/clientController.js
@@ -85,10 +85,28 @@ const getClientDetails = async (req, res) => {
     res.status(500).json({ success: false, message: "Invalid User" });
   }
 }
+
+
+const updateProfile = async (req,res)=>{
+  try {
+    console.log(req.body)
+
+    const user = await User.findById(req.userId);
+    if(!user) return res.status(404).json({ success: false, message: "User not found" });
+
+    user.fullname = req.body.fullname;
+    user.phone = req.body.phone
+    await user.save();
+    res.status(200).json({ success: true, user });
+  } catch (error) {
+    res.status(500).json({ success: false, message: "Failed to update profile" });
+  }
+}
 module.exports = {
   getTopServices,
   getServices,
   getFilterData,
   getService,
-  getClientDetails
+  getClientDetails,
+  updateProfile
 };
