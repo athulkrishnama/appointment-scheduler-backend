4691e68008815c9d0159b6969cd90b2bd42205ec
athulkrishnama <athulkrishnama24@gmail.com>
2025-01-21 11:52:05 +0530

    new field added for document

--- models/user.js
+++ models/user.js
@@ -49,6 +49,10 @@ const userSchema = new mongoose.Schema(
         logo: {
           type: String,
         },
+        document:{
+          type: String
+        }
+        ,
       }),
       default: null,
       required: function () {
