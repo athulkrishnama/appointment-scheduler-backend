5da64a58ce31ff39c00690edf859a987d38fae76
athulkrishnama <athulkrishnama24@gmail.com>
2025-01-25 11:52:17 +0530

    quotation sending implemented

--- controllers/client/serviceRequestController.js
+++ controllers/client/serviceRequestController.js
@@ -1,7 +1,11 @@
-
+const ServiceRequest = require('../../models/serviceRequests');
 const serviceRequest =async (req, res)=>{
     try{
-        console.log(req.body);
+        const {address, service, date, time, additionalNotes, additionalDetails, recrringEndDate, serviceFrequency} = req.body;
+        const client = req.userId;
+        const serviceRequest = new ServiceRequest({address, service, date, time, additionalNotes, additionalDetails, recrringEndDate, serviceFrequency, client});
+        await serviceRequest.save();
+        res.status(201).json({ success: true, message: "Service request created successfully", serviceRequest });
     }catch(err){
         console.log(err);
         res.status(500).json({ success: false, message: "Failed to create service request" });
--- models/serviceRequests.js
+++ models/serviceRequests.js
@@ -1,21 +1,77 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
-const serviceRequestSchema = new mongoose.Schema({
-    client:{
-        type: mongoose.Schema.Types.ObjectId,
-        ref: 'User',
-        required: true
+const serviceRequestSchema = new mongoose.Schema(
+  {
+    status: {
+      type: String,
+      enum: ["pending", "accepted", "rejected"],
+      default: "pending",
     },
-    addressId:{
-        type: mongoose.Schema.Types.ObjectId,
-        ref: 'Address',
-        required: true
+    client: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
     },
-    service:{
-        type: mongoose.Schema.Types.ObjectId,
-        required: true
-    }
-    
-});
+    address: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Address",
+      required: true,
+    },
+    service: {
+      type: mongoose.Schema.Types.ObjectId,
+      required: true,
+    },
+    date: {
+      type: Date,
+      required: true,
+    },
+    time: {
+      type: String,
+      required: true,
+    },
+    additionalNotes: {
+      type: String,
+      required: true,
+    },
+    additionalDetails: [
+      {
+        fieldName: {
+          type: String,
+          required: [true, "Field name is required"],
+        },
+        fieldType: {
+          type: String,
+          enum: ["number", "string"],
+          required: [true, "Field type is required"],
+        },
+        value:{
+            type: String,
+            required: [true, "Value is required"],
+        }
+      },
+    ],
+    recrringEndDate: {
+      type: Date,
+    },
+    serviceFrequency: {
+      type: String,
+      enum: [
+        'Once',
+        'Weekly Twice',
+        'Daily',
+        'Weekly',
+        'Fortnightly',
+        'Monthly',
+        'Quarterly',
+        'Half Yearly',
+        'Yearly'
+      ],
+      required: [true, "Service frequency is required"],
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
 
-module.exports = mongoose.model('ServiceRequest', serviceRequestSchema);
+module.exports = mongoose.model("ServiceRequest", serviceRequestSchema);
