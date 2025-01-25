8871df56da9bd588f3beab8e3d3bfd2e3755c0fd
athulkrishnama <athulkrishnama24@gmail.com>
2025-01-25 17:42:09 +0530

    controller created for getting service request

--- controllers/client/serviceRequestController.js
+++ controllers/client/serviceRequestController.js
@@ -1,9 +1,9 @@
 const ServiceRequest = require('../../models/serviceRequests');
 const serviceRequest =async (req, res)=>{
     try{
-        const {address, service, date, time, additionalNotes, additionalDetails, recrringEndDate, serviceFrequency} = req.body;
+        const {address, service, date, time, additionalNotes, additionalDetails, endDate, serviceFrequency} = req.body;
         const client = req.userId;
-        const serviceRequest = new ServiceRequest({address, service, date, time, additionalNotes, additionalDetails, recrringEndDate, serviceFrequency, client});
+        const serviceRequest = new ServiceRequest({address, service, date, time, additionalNotes, additionalDetails, recrringEndDate:endDate, serviceFrequency, client});
         await serviceRequest.save();
         res.status(201).json({ success: true, message: "Service request created successfully", serviceRequest });
     }catch(err){
@@ -12,6 +12,21 @@ const serviceRequest =async (req, res)=>{
     }
 }
 
+const getServiceRequests = async (req, res)=>{
+    try {
+        const client = req.userId;
+        const page = parseInt(req.query.page) || 1;
+        const limit = parseInt(req.query.limit) || 5;
+        const serviceRequests = await ServiceRequest.find({client}).sort({createdAt:-1}).skip((page - 1) * limit).limit(limit).populate('service');
+        const totalPages = Math.ceil((await ServiceRequest.countDocuments({client}))/limit);
+        res.status(200).json({ success: true, serviceRequests, totalPages });
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({ success: false, message: "Failed to get service requests" });
+    }
+}
+
 module.exports={
-    serviceRequest
+    serviceRequest,
+    getServiceRequests
 }
