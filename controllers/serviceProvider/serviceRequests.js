9cfed26306197e0454aa5d40dc43f1029b353f11
athulkrishnama <athulkrishnama24@gmail.com>
2025-01-26 18:31:11 +0530

    created new controller for getting data of single service request , create a quotation and getting chat inside a service request.

--- controllers/serviceProvider/serviceRequests.js
+++ controllers/serviceProvider/serviceRequests.js
@@ -1,4 +1,8 @@
 const ServiceRequest = require('../../models/serviceRequests');
+const Quotation = require('../../models/quotations');
+const Chat = require('../../models/chat');
+const types = require('../../constants/chatType');
+const sender = require('../../constants/sender');
 
 const getServiceRequests = async (req, res) => {
     try {
@@ -24,6 +28,61 @@ const getServiceRequests = async (req, res) => {
     }
 }
 
+const getServiceRequest = async (req, res) => {
+    try {
+        const serviceRequestId = req.params.id;
+        const serviceRequest = await ServiceRequest.findById(serviceRequestId).populate([{
+            path: 'service',
+            // match: { serviceProvider: serviceProvider }
+        },{
+            path: 'client'
+        }]);
+        res.status(200).json({ success: true, serviceRequest });
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({ success: false, message: "Failed to get service request" });
+    }
+}
+
+const createQuotation = async (req, res) => {
+    try {
+        const requestId = req.params.id;
+        const amountBreakdown = req.body;
+        const quotation = await new Quotation({
+            serviceRequest: requestId,
+            amountBreakdown
+        });
+        const newQuotation = await quotation.save();
+        if(newQuotation){
+            const chat = await new Chat({
+                messageType: types.quotation,
+                message: newQuotation._id,
+                serviceRequest: requestId,
+                sender: sender.serviceProvider
+            });
+            await chat.save();
+        }
+        res.status(200).json({ success: true, message: "Quotation created successfully" });
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({ success: false, message: "Failed to create quotation" });
+    }
+}
+
+const getChat = async (req, res) => {
+    try {
+        const requestId = req.params.id;
+        const chat = await Chat.find({ serviceRequest: requestId }).populate('message');
+        res.status(200).json({ success: true, chat });
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({ success: false, message: "Failed to get chat" });
+    }
+}
+
 module.exports = {
-    getServiceRequests
+    getServiceRequests,
+    getServiceRequest,
+    createQuotation,
+    getChat
 }
