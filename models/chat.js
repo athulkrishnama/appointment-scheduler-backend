177df6568e734c16be2c1b7c891c4a91b6173c3e
athulkrishnama <athulkrishnama24@gmail.com>
2025-01-26 18:31:27 +0530

    chat modal created

---
+++ models/chat.js
@@ -0,0 +1,42 @@
+const mongoose = require("mongoose");
+
+const chatSchema = new mongoose.Schema({
+    serviceRequest: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "ServiceRequest",
+        required: true,
+        autopopulate: true
+    },
+    messageType: {
+        type: String,
+        enum: ["text", "quotation"],
+        required: true,
+    },
+    message: {
+        type: mongoose.Schema.Types.Mixed,
+        required: true,
+        validate: {
+            validator: function (value) {
+                return typeof value === 'string' || (typeof value === 'object' && value instanceof mongoose.Types.ObjectId);
+            },
+            message: 'Message must be a string or a valid reference to a quotation model.'
+        },
+        ref: 'Quotation',
+        autopopulate: true
+    },
+    sender: {
+        type:String,
+        enum: ["client", "service provider"],
+        required: true,
+    },
+    // for implementing status in future
+    // status: {
+    //     type: String,
+    //     enum: ["sent", "received"],
+    //     default: "sent",
+    //     required: true,
+    // }
+
+}, { timestamps: true });
+
+module.exports = mongoose.model("Chat", chatSchema);
