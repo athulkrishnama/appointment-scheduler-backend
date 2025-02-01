e0c127c0dcbd8ab68bf486fbb7f633815b4b9af9
athulkrishnama <athulkrishnama24@gmail.com>
2025-02-01 11:10:34 +0530

    created cancel appointment controller

--- controllers/client/appointmentController.js
+++ controllers/client/appointmentController.js
@@ -14,6 +14,29 @@ const getAppointments = async (req, res) => {
     }
 }
 
+// 
+const cancelAppointment = async (req, res) => {
+    try {
+        const { id } = req.params;
+        const {reason} = req.body;
+        const appointment = await Appointment.findById(id);
+        if (!appointment) {
+            return res.status(404).json({ success: false, message: "Appointment not found" });
+        }
+        if(appointment.client.toString() !== req.userId){
+            return res.status(403).json({ success: false, message: "You are not authorized to cancel this appointment" });
+        }
+        appointment.status = 'cancelled';
+        appointment.cancellationReason = reason;
+        await appointment.save();
+        res.status(200).json({ success: true, message: "Appointment cancelled successfully" });
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ success: false, message: "Internal server error" });
+    }
+}
+
 module.exports = {
-    getAppointments
+    getAppointments,
+    cancelAppointment
 }
