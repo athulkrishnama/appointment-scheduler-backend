10d15d5732c79f0d5fb9ce5e79ce8de59704ef13
athulkrishnama <athulkrishnama24@gmail.com>
2025-02-04 15:48:50 +0530

    commision deduct type added

--- constants/transactionType.js
+++ constants/transactionType.js
@@ -2,7 +2,8 @@ const transactionTypes = {
     SERVICE_PAYMENT: "service_payment",
     REFUND: "refund",
     WITHDRAWAL: "withdrawal",
-    DEPOSIT: "deposit"
+    DEPOSIT: "deposit",
+    COMMISSION_DEDUCTION: "commission_deduction"
 }
 
 module.exports = transactionTypes
