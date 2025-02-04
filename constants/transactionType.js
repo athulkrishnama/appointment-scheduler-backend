29e7391642566fb158cd4cbb7ac9d411140846a7
athulkrishnama <athulkrishnama24@gmail.com>
2025-02-04 10:52:54 +0530

    constant file for storing transaction type

---
+++ constants/transactionType.js
@@ -0,0 +1,8 @@
+const transactionTypes = {
+    SERVICE_PAYMENT: "service_payment",
+    REFUND: "refund",
+    WITHDRAWAL: "withdrawal",
+    DEPOSIT: "deposit"
+}
+
+module.exports = transactionTypes
