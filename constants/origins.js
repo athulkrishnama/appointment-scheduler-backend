524f2d556eefeb46e1b549ec5c137b3ee61ae507
athulkrishnama <athulkrishnama24@gmail.com>
2025-02-11 19:22:51 +0530

    socket origin added

--- constants/origins.js
+++ constants/origins.js
@@ -2,5 +2,6 @@ const ORIGIN = {
     client:process.env.CLIENT_ORIGIN,
     service:process.env.SERVICE_PROVIDER_ORIGIN,
     admin:process.env.ADMIN_ORIGIN,
+    socket:process.env.SOCKET_ORIGIN
 }
 module.exports = ORIGIN;
