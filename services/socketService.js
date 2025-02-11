24fcee2196b98552eb36a961f8aeb18a1ac75120
athulkrishnama <athulkrishnama24@gmail.com>
2025-02-11 19:29:47 +0530

    admin ui authentication added

--- services/socketService.js
+++ services/socketService.js
@@ -1,14 +1,26 @@
 const chatSocket = require("../chat/chatSocket");
+const {instrument} = require('@socket.io/admin-ui')
+const origins = require("../constants/origins");
 
 const initializeSocket = (server) => {
     const io = require("socket.io")(server, {
         cors: {
-            origin: "*",
-            methods: ["GET", "POST"],
+            origin:[ origins.client, origins.service, origins.admin, origins.socket],
+            credentials: true,
         },
     });
 
     chatSocket(io);
+    instrument(io, {
+        auth: {
+            type:"basic",
+            username:process.env.SOCKET_USERNAME,
+            password:process.env.SOCKET_PASSWORD
+        },
+        // namespacename:"/socket"
+        mode:'development',
+    })
+    
     return io;
 }
 
