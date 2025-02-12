8e2f4fa399d225835f4fbb65b27d8a0e1b85df12
athulkrishnama <athulkrishnama24@gmail.com>
2025-02-12 19:50:56 +0530

    notification sending feature added when a message is received

--- chat/chatSocket.js
+++ chat/chatSocket.js
@@ -1,14 +1,22 @@
 const helper = require("./helper");
-const chatSocket = (io)=>{
+const {getSocketId, getName} = require('../notification/helper');
+const chatType = require("../constants/chatType");
+
+
+const chatSocket = (io, notificationIo)=>{
     io.on("connection", (socket)=>{
         console.log("a user connected");
         socket.emit("new", "new");
 
         socket.on("sendMessage", async (data, callback)=>{
             try{
-
                 const newChat = await helper.saveTextMessage(data);
                 io.to(data.room).emit("receiveMessage", newChat);
+                const receiverSocketId = getSocketId(data.receiverId);
+                if(receiverSocketId){
+                    const senderName = await getName(data.senderId);
+                    notificationIo.to(receiverSocketId).emit("newNotification", {id:newChat._id,serviceRequest: data.room, sender: senderName, messageType: chatType.text});                    
+                }
                 callback({success:true, chat: newChat});
 
             }catch(err){
@@ -24,6 +32,11 @@ const chatSocket = (io)=>{
                     return callback({success:false, message: "Something went wrong"});
                 }
                 io.to(data.room).emit("receiveMessage", newChat);
+                const receiverSocketId = getSocketId(data.receiverId);
+                if(receiverSocketId){
+                    const senderName = await getName(data.senderId);
+                    notificationIo.to(receiverSocketId).emit("newNotification", {id:newChat._id,serviceRequest: data.room, sender: senderName, messageType: chatType.quotation});                    
+                }
                 callback({success:true, chat: newChat});
             }catch(err){
                 console.log(err);
@@ -38,7 +51,8 @@ const chatSocket = (io)=>{
 
         socket.on("disconnect", (data)=>{
             socket.leave(data.room);
-            console.log("a user disconnected", data.room);
+
+            console.log("a user disconnected", data);
         })
     })
 }
